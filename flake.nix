1b4e6e1ed6d78fa9c323ba98f28c8ad6a6882aab
Ryan Butler <thebutlah@gmail.com>
2025-12-19 13:11:42 -0800

    chore(nix): added nix flake

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.orb-software.url = "github:worldcoin/orb-software";
+
+  outputs = { self, flake-utils, nixpkgs, orb-software, ... }:
+    # Now eachDefaultSystem is only using ["x86_64-linux"], but this list can also
+    # further be changed by users of your flake.
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default = orb-software.devShells.${system}.default;
+    });
+}
