42e77d85c624169774a0c0e4d664bec5a72aca77
Ryan Butler <thebutlah@gmail.com>
2026-01-07 20:24:30 -0500

    fix(ci): dont actually populate seekSdk from nix (#17)
    Since we don't actually use the seek sdk in this repo, and since that
    dependency is private and complicates the github token, we intentionally
    override it to an empty directory.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,18 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.orb-software.url = "github:worldcoin/orb-software";
+  inputs.orb-software = {
+    url = "github:worldcoin/orb-software";
+    inputs.seekSdk.url = "path:./empty";
+  };
 
-  outputs = { self, flake-utils, nixpkgs, orb-software, ... }:
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      orb-software,
+      ...
+    }:
     # Now eachDefaultSystem is only using ["x86_64-linux"], but this list can also
     # further be changed by users of your flake.
     flake-utils.lib.eachDefaultSystem (system: {
