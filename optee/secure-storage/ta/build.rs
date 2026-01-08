13ebc6bdd8020d937cfb4aefcebc356a1bdfdddd
Ryan Butler <thebutlah@gmail.com>
2026-01-07 21:45:06 -0500

    fix(secure-storage): encode commit as TA version (#20)
    Previously we were using the crate version as the TA version, which is
    not how orb-rustzone does versioning.

--- optee/secure-storage/ta/build.rs
+++ optee/secure-storage/ta/build.rs
@@ -1,8 +1,25 @@
+use std::process::Command;
+
 use optee_utee_build::{Error, RustEdition, TaConfig};
 
 fn main() -> Result<(), Error> {
-    let config = TaConfig::new_default_with_cargo_env(
+    let mut config = TaConfig::new_default_with_cargo_env(
         orb_secure_storage_proto::StorageDomain::WifiProfiles.as_uuid(),
     )?;
+    config.ta_version = git_rev_parse();
+
     optee_utee_build::build(RustEdition::Edition2024, config)
+}
+
+fn git_rev_parse() -> String {
+    let stdout = Command::new("git")
+        .args(["rev-parse", "HEAD"])
+        .output()
+        .expect("git should be installed")
+        .stdout;
+    let cleaned_stdout = std::str::from_utf8(&stdout)
+        .expect("git command should be valid utf8")
+        .trim();
+
+    cleaned_stdout.to_owned()
 }
